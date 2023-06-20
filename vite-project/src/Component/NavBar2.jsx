84c4ebc35994514ae789510cdfba3db4dd190121
Reema <reema600@hotmail.com>
2023-06-20 17:51:55 +0300

    update

--- vite-project/src/Component/NavBar2.jsx
+++ vite-project/src/Component/NavBar2.jsx
@@ -1,4 +1,4 @@
-import { useRef,useState } from "react";
+import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "../Component/NavBar.css";
 // import { useNavigate } from 'react-router-dom'
