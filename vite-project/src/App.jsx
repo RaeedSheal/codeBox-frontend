df961ea26cd061ae0ef5fdf0b1421754f3e891bc
Raeed <91532317+RaeedSheal@users.noreply.github.com>
2023-06-20 08:22:02 +0300

    Linked sign up and login
    Urls are connected to render

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,27 @@
-
-import './App.css'
-import { Route,Routes} from "react-router-dom";
-import Login from './Component/Login';
-import LandingPage2 from './Pages/LandingPage2';
-import SingnUp2 from './Component/SingnUp2';
-import Home from './Pages/Home';
-import BoxIdea from './Pages/BoxIdea'
-import ResetPassword from './Component/ResetPassword';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Component/Login";
+import LandingPage2 from "./Pages/LandingPage2";
+import SingnUp2 from "./Component/SingnUp2";
+import Home from "./Pages/Home";
+import BoxIdea from "./Pages/BoxIdea";
+import ResetPassword from "./Component/ResetPassword";
 function App() {
-
-
-  return (
-    <>
- 
-
-      
-      <Routes>
-        
-      <Route path='/' element={<LandingPage2/>}></Route>
-      <Route path='/Home' element={<Home/>}></Route>
-      <Route path='/Box' element={<BoxIdea/>}></Route>
-      <Route path='/SignUp2' element={<SingnUp2/>}></Route>
-        <Route path='/Login' element={<Login/>}></Route>
-        <Route path='/ResetPassword' element={<ResetPassword/>}></Route>
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<LandingPage2 />}></Route>
+                <Route path="/Home" element={<Home />}></Route>
+                <Route path="/Box" element={<BoxIdea />}></Route>
+                <Route path="/SignUp2" element={<SingnUp2 />}></Route>
+                <Route path="/Login" element={<Login />}></Route>
+                <Route
+                    path="/ResetPassword"
+                    element={<ResetPassword />}
+                ></Route>
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
