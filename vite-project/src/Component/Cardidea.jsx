6efe909d28f35bda5eb09b90c6d2f46eca76c609
Raeed <91532317+RaeedSheal@users.noreply.github.com>
2023-06-21 06:40:13 +0300

    Linked View Ideas +Delete ideas

---
+++ vite-project/src/Component/Cardidea.jsx
@@ -0,0 +1,134 @@
+import {
+    Heading,
+    Button,
+    Box,
+    Stack,
+    StackDivider,
+    CardHeader,
+    Card,
+    CardBody,
+    Text,
+    CardFooter,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const Cardidea = (props) => {
+    const DeleteItem = () => {
+        axios
+            .delete(`http://localhost:8000/api/deleleidea/${props.id}`, {
+                withCredentials: true,
+            })
+            .then((res) => {
+                console.log(res);
+            });
+    };
+    return (
+        <>
+            <Card>
+                <CardHeader bgColor="#fdfff0">
+                    <Heading size="md" color="#8d622f">
+                        {props.name}
+                    </Heading>
+                </CardHeader>
+
+                <CardBody bgColor="#fdfff0">
+                    <Stack divider={<StackDivider />} spacing="4">
+                        <Box>
+                            <Heading
+                                size="xs"
+                                textTransform="uppercase"
+                                color="#8d622f"
+                            >
+                                Instructions
+                            </Heading>
+                            <Text pt="2" fontSize="sm" color="#D5C39B">
+                                {props.instructions}.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading
+                                size="xs"
+                                textTransform="uppercase"
+                                color="#8d622f"
+                            >
+                                Input A
+                            </Heading>
+                            <Text pt="2" fontSize="sm" color="#D5C39B">
+                                {props.InputA}.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading
+                                size="xs"
+                                textTransform="uppercase"
+                                color="#8d622f"
+                            >
+                                Input B
+                            </Heading>
+                            <Text pt="2" fontSize="sm" color="#D5C39B">
+                                {props.InputB}.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading
+                                size="xs"
+                                textTransform="uppercase"
+                                color="#8d622f"
+                            >
+                                Output A
+                            </Heading>
+                            <Text pt="2" fontSize="sm" color="#D5C39B">
+                                {props.OutputA}.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading
+                                size="xs"
+                                textTransform="uppercase"
+                                color="#8d622f"
+                            >
+                                Output B
+                            </Heading>
+                            <Text pt="2" fontSize="sm" color="#D5C39B">
+                                {props.OutputB}.
+                            </Text>
+                        </Box>
+                    </Stack>
+                </CardBody>
+                <CardFooter bgColor="#fdfff0" gap={"10px"}>
+                    <Link to="/EditIdea">
+                        <Button
+                            borderRadius={10}
+                            type="submit"
+                            variant="solid"
+                            bgColor="#D5C39B"
+                            color="#8d622f"
+                            w={"full"}
+                            _hover={{ bg: "#8d622f", color: " white" }}
+                        >
+                            Edit{" "}
+                        </Button>
+                    </Link>
+                    <br />
+                    <Button
+                        borderRadius={10}
+                        type="submit"
+                        variant="solid"
+                        bgColor="#D5C39B"
+                        color="#8d622f"
+                        w={"full"}
+                        _hover={{ bg: "#8d622f", color: " white" }}
+                        onClick={() => {
+                            DeleteItem();
+                        }}
+                    >
+                        delete{" "}
+                    </Button>{" "}
+                    <br />
+                </CardFooter>
+            </Card>
+        </>
+    );
+};
+
+export default Cardidea;
--- vite-project/src/Component/ViewIdeas.jsx
+++ vite-project/src/Component/ViewIdeas.jsx
@@ -1,193 +1,59 @@
-import {
-  
-    Heading,
-    Button,
-    Box,
-    Stack,
-    StackDivider,
-    CardHeader,
-    Card,
-    CardBody,
-    Text,
-    Center,
-    CardFooter
-  } from "@chakra-ui/react";
-  import { Link} from "react-router-dom";
+import { Heading, Stack, Center, Grid } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { FcIdea } from "react-icons/fc";
-  import Footer from '../Component/Footer'
-  import NavBar2 from '../Component/NavBar2'
+import Footer from "../Component/Footer";
+import NavBar2 from "../Component/NavBar2";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cardidea from "./Cardidea";
 
 export default function ViewIdeas() {
-  return (
-    <>
- <NavBar2></NavBar2>
- <Center paddingTop='20px' bgColor='#fdfff0'>
-
-        <Heading color="#8d622f" >All Idea   </Heading>
-        <FcIdea bg="#D5C39B" size='50px'/>
-        </Center>
- <Stack spacing='8' bgColor='#fdfff0' >
- <Center paddingTop='100px'  paddingBottom='400px' paddingLeft='20px' paddingRight='20px' gap={6}>
-   <Card >
-  <CardHeader bgColor='#fdfff0'>
-    
-    <Heading size='md' color="#8d622f">Idea Name</Heading>
-  </CardHeader>
-
-  <CardBody bgColor='#fdfff0'>
-    <Stack divider={<StackDivider />} spacing='4'>
-      <Box>
-        <Heading size='xs' textTransform='uppercase' color="#8d622f">
-        Instructions
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Instructions.
-        </Text>
-        
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Input A
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Input A.
-        </Text>
-  
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Input B
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Input B.
-        </Text>
-       
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Output A
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Output A.
-        </Text>
-       
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Output B
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Output B.
-        </Text>
-       
-      </Box>
-    </Stack>
-  </CardBody>
-  <CardFooter bgColor='#fdfff0' gap={'10px'}>
-    <Link to='/EditIdea'>
-  <Button  
-  borderRadius={10}
-   type="submit"
-    variant="solid"
-    bgColor="#D5C39B"
-    color="#8d622f"
-    w={"full"}
-    _hover={{ bg: "#8d622f", color: " white" }}
-   
->Edit </Button> 
-</Link>
-<br/>
-  <Button       borderRadius={10}
-   type="submit"
-    variant="solid"
-    bgColor="#D5C39B"
-    color="#8d622f"
-    w={"full"}
-    _hover={{ bg: "#8d622f", color: " white" }}
->delete </Button> <br/>
-</CardFooter>
-</Card>
-<Card >
-  <CardHeader bgColor='#fdfff0'>
-    
-    <Heading size='md' color="#8d622f">Idea Name</Heading>
-  </CardHeader>
-
-  <CardBody bgColor='#fdfff0'>
-    <Stack divider={<StackDivider />} spacing='4'>
-      <Box>
-        <Heading size='xs' textTransform='uppercase' color="#8d622f">
-        Instructions
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Instructions.
-        </Text>
-        
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Input A
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Input A.
-        </Text>
-  
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Input B
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Input B.
-        </Text>
-       
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Output A
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Output A.
-        </Text>
-       
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'  color="#8d622f">
-        Output B
-        </Heading>
-        <Text pt='2' fontSize='sm' color="#D5C39B">
-        Output B.
-        </Text>
-       
-      </Box>
-    </Stack>
-  </CardBody>
-  <CardFooter bgColor='#fdfff0' gap={'10px'}>
-    <Link to='/EditIdea'>
-  <Button      borderRadius={10}
-   type="submit"
-    variant="solid"
-    bgColor="#D5C39B"
-    color="#8d622f"
-    w={"full"}
-    _hover={{ bg: "#8d622f", color: " white" }}
->Edit </Button>
-</Link>
- <br/>
-  <Button      borderRadius={10}
-   type="submit"
-    variant="solid"
-    bgColor="#D5C39B"
-    color="#8d622f"
-    w={"full"}
-    _hover={{ bg: "#8d622f", color: " white" }}
->delete </Button> <br/>
-</CardFooter>
-</Card>
-</Center>
-
-</Stack>
-    <Footer></Footer>
-    </>
-  )
+    const [ideas, setIdeas] = useState([]);
+    const url = "http://localhost:8000/api/ideas";
+    useEffect(() => {
+        axios.get(url, { withCredentials: true }).then((res) => {
+            setIdeas(res.data.ideas);
+            console.log(res.data.ideas);
+        });
+    }, []);
+    return (
+        <>
+            <NavBar2></NavBar2>
+            <Center paddingTop="20px" bgColor="#fdfff0">
+                <Heading color="#8d622f">All Idea </Heading>
+                <FcIdea bg="#D5C39B" size="50px" />
+            </Center>
+            <Stack spacing="8" bgColor="#fdfff0">
+                <Center
+                    paddingTop="100px"
+                    paddingBottom="400px"
+                    paddingLeft="20px"
+                    paddingRight="20px"
+                    gap={6}
+                >
+                    <Grid
+                        gap={4}
+                        templateColumns={{ base: "1fr", md: "repeat(4,1fr)" }}
+                        m={4}
+                    >
+                        {ideas.map((idea) => {
+                            return (
+                                <Cardidea
+                                    key={idea._id}
+                                    id={idea._id}
+                                    name={idea.name}
+                                    instructions={idea.instructions}
+                                    InputA={idea.inputA}
+                                    InputB={idea.inputB}
+                                    OutputA={idea.outputA}
+                                    OutputB={idea.outputB}
+                                />
+                            );
+                        })}
+                    </Grid>
+                </Center>
+            </Stack>
+            <Footer></Footer>
+        </>
+    );
 }
